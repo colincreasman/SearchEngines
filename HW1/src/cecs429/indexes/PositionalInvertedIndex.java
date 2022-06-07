bcb49168d87d4d33f7bd0b76cbefabf0e47f1621
colincreasman <71061172+colincreasman@users.noreply.github.com>
2022-06-07 15:04:03 -0700

    Finished PositionalInvertedIndex
    ( i think)

--- HW1/src/cecs429/indexes/PositionalInvertedIndex.java
+++ HW1/src/cecs429/indexes/PositionalInvertedIndex.java
@@ -1,10 +1,58 @@
 package cecs429.indexes;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 public class PositionalInvertedIndex implements Index {
     private final HashMap<String, List<Posting>> mIndex;
+    private final List<String> mVocabulary;
+
+    public PositionalInvertedIndex(Collection<String> vocabulary) {
+        mVocabulary = new ArrayList<String>();
+        mVocabulary.addAll(vocabulary);
+        Collections.sort(mVocabulary);
+
+        mIndex = new HashMap<>();
+    }
+
+    /**
+     * Adds a term to the index using the document it occurs in and a list of integer positions where it occurs
+     * @param term
+     * @param documentId
+     * @param termPosition
+     */
+    public void addTerm(String term, int documentId, int termPosition) {
+        // first check if the term is already in the index by trying to access its postings in the index
+        List<Posting> postingsInIndex = mIndex.get(term);
+
+        // if the value is not null, the term must already be in the index
+        if (postingsInIndex != null) {
+            // now check if the term already has a Posting for this specific docId
+            // find the last posting for the term in the HashMap index
+            Posting lastPosting = postingsInIndex.get(postingsInIndex.size() - 1);
+
+            // since the postings lists are always sorted, if the current docId is in the term's postings list it would be the last item added to the list
+            if (lastPosting.getDocumentId() == documentId) {
+                // if true, then the term's postings list already contains a posting for this doc, so we add the new termPosition to the end of the postings list for the term
+                lastPosting.addTermPosition(termPosition);
+            }
+            else {
+                // if false, then the posting list for this term exists but does not yet contain a posting for this doc
+                Posting newPosting = new Posting(documentId, termPosition);
+                // add a new posting with the docId and termPosition to the HashMap index
+                postingsInIndex.add(newPosting);
+            }
+        }
+        // if the val for this term is null, the term has not been added to the HashMap index yet
+        else {
+            // create a postings list and add a new posting with the docId and termPosition to it
+            List<Posting> newPostingsList = new ArrayList<>();
+            Posting newPosting = new Posting(documentId, termPosition);
+            newPostingsList.add(newPosting);
+            // add the new term and new postingsList to the HashMap index
+            mIndex.put(term, newPostingsList);
+        }
+
+    }
 
 
     /**
--- HW1/src/cecs429/indexes/Posting.java
+++ HW1/src/cecs429/indexes/Posting.java
@@ -8,7 +8,29 @@ import java.util.*;
 public class Posting {
 	private int mDocumentId;
 	private List<Integer> mTermPositions;
-	
+	/**
+	 * Simple constructor for making a posting with only a documentId at the time of initialization
+	 * @param documentId
+	 */
+	public Posting(int documentId) {
+		mDocumentId = documentId;
+		mTermPositions = new ArrayList<>();
+	}
+	/**
+	 * Overloaded constructor for making a posting with only a single termPosition at the time of initialization
+	 * @param documentId
+	 * @param termPosition
+	 */
+	public Posting(int documentId, int termPosition) {
+		mDocumentId = documentId;
+		mTermPositions = new ArrayList<>();
+		mTermPositions.add(termPosition);
+	}
+	/**
+	 * Overloaded constructor for making a posting with only a a list of termPosition at the time of initialization
+	 * @param documentId
+	 * @param termPositions
+	 */
 	public Posting(int documentId, List<Integer> termPositions) {
 		mDocumentId = documentId;
 		mTermPositions = termPositions;
@@ -20,5 +42,10 @@ public class Posting {
 
 	public List<Integer> getTermPositions() {
 		return mTermPositions;
+	}
+
+	public void addTermPosition (int position) {
+		mTermPositions.add(position);
+
 	}
 }
